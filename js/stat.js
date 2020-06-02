fe6a641367553ea1547850f6d494e316d7ea8d6c
leleo21 <ks050795@mail.ru>
2020-06-02 20:25:48 +0300

    Добавляет облако

---
+++ js/stat.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+var renderCloud = function (ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
+window.renderStatistics = function (ctx) {
+  renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.3)');
+  renderCloud(ctx, 100, 10, '#fff');
+
+  ctx.fillStyle = '#000';
+  ctx.font = '16px PT Mono';
+  ctx.fillText('Ура вы победили!', 120, 40);
+  ctx.fillText('Список результатов:', 120, 60);
+};
